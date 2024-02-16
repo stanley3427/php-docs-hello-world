6109efd3438b7e9b328f9281ac8f489c7cdadac5
stanley3427 <83268419+stanley3427@users.noreply.github.com>
2024-02-16 16:07:46 +0800

    new

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-echo "<pre>";
-echo phpinfo();
-echo "</pre>";
+if (empty($_POST['user']) == True)
+{
+	echo "<form action=\"#\" method=\"POST\">";
+	echo "What do you want to say ? <input type=\"text\" id=\"user\" name=\"user\" autocomplete=\"off\" required autofocus>&nbsp;";
+	echo "<input type=\"submit\" >&nbsp;";
+	echo "<input type=\"reset\" >";
+	echo "</form>";
+}
+else
+{
+	if (strtolower($_POST['user']) == "wade")
+	{
+		echo "<pre>";
+		echo phpinfo();
+		echo "</pre>";
+	}
+	else
+	{
+		echo "<p>";
+		echo "You say [".htmlentities($_POST['user'])."]";
+		echo "</p>";
+	}
+	echo "<input type=\"button\" value=\"Back\" onClick=\"history.go(-1)\" autofocus>";
+}
 ?>
